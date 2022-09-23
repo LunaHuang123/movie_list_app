0fe35729cdf9c984f00e9226946526b0a07da8cb
Qi Luan <16888917+qluan1@users.noreply.github.com>
2022-09-23 11:03:44 -0700

    recovered import Provider

--- movie_list/src/index.js
+++ movie_list/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { fetchSavePage } from './actionCreators/pageActionCreator';
 
