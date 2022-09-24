7e3e71b89eb5909e1694e82516a7b9f3db069d7e
Andonidas <yanyul4@uci.edu>
2022-09-24 08:44:33 -0700

    apply another simple test in index.js

--- movie_list/src/index.js
+++ movie_list/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
-// import { fetchSaveMovie } from './actionCreators/movieDetailsActionCreator';
+import { fetchSaveMovie } from './actionCreators/movieDetailsActionCreator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = configureStore();
@@ -16,10 +16,10 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-// store.dispatch(fetchSaveMovie(1));
-// store.dispatch(fetchSaveMovie(2));
-// store.dispatch(fetchSaveMovie(2));
-// setTimeout(() => console.log(store.getState()), 3000);
+store.dispatch(fetchSaveMovie(1));
+store.dispatch(fetchSaveMovie(2));
+store.dispatch(fetchSaveMovie(2));
+setTimeout(() => console.log(store.getState()), 3000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
