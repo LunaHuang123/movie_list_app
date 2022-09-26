7fc1be06c56a7ed52e6804b27c0906b3479ec679
Andonidas <yanyul4@uci.edu>
2022-09-26 14:52:22 -0700

    further tests on reference, use new reference for both lists in like or block action

--- movie_list/src/reducers/like_block_reducer.js
+++ movie_list/src/reducers/like_block_reducer.js
@@ -12,30 +12,46 @@ const likeBlockReducer = (state = initialState, action) => {
     case (ACTION_TYPES.likedList.addMovie): {
         if(newState.blockedList[action.payload.movieID]) delete newState.blockedList[action.payload.movieID];
         return {
-        ...newState,
-        likedList: {
-            ...newState.likedList,
-            [action.payload.movieID]: action.payload.pageNum
-        }
+            ...newState,
+            likedList: {
+                ...newState.likedList,
+                [action.payload.movieID]: action.payload.pageNum
+            },
+            blockedList: {
+                ...newState.blockedList
+            }
         };
     }
     case (ACTION_TYPES.likedList.removeMovie): {
         delete newState.likedList[action.payload.movieID];
-        return newState;
+        return {
+            ...newState,
+            likedList: {
+                ...newState.likedList
+            }
+        };
     } 
     case (ACTION_TYPES.blockedList.addMovie): {
         if(newState.likedList[action.payload.movieID]) delete newState.likedList[action.payload.movieID];
         return {
-        ...newState,
-        blockedList: {
-            ...newState.blockedList,
-            [action.payload.movieID]: action.payload.pageNum
-        }
+            ...newState,
+            likedList: {
+                ...newState.likedList
+            },
+            blockedList: {
+                ...newState.blockedList,
+                [action.payload.movieID]: action.payload.pageNum
+            }
         };
     }
     case (ACTION_TYPES.blockedList.removeMovie): {
         delete newState.blockedList[action.payload.movieID];
-        return newState;
+        return {
+            ...newState,
+            blockedList: {
+                ...newState.blockedList
+            }
+        };
     }  
     default : 
       return state;
