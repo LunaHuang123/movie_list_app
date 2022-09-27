122dc232c7f18cb209d36a0acb771b4f820fc867
Andonidas <yanyul4@uci.edu>
2022-09-27 12:46:50 -0700

    Fixed import file structure

--- movie_list/src/App.js
+++ movie_list/src/App.js
@@ -1,33 +1,12 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import TopRatedPage from './pages/top_rated_page';
-import MovieModal from './components/movie_modal';
-import Sidebar from './components/sidebar/sidebar';
-import HomePage from './components/homepage/homepage_container'
+
+import MovieList from './components/movieListComponent/layout' ;
 
 function App() {
   const [showOption,setShow] = useState({show:false,movieID:null});
   return (
-    // <div className="App">
-    //   <Router>
-    //     <Sidebar/>
-    //   </Router>
-    //   <button onClick={()=>{setShow({show:true,movieID:2})}}>{/*should receive ID from props.movieID*/}
-    //     movie 1
-    //   </button>
-    //   <button onClick={()=>{setShow({show:true,movieID:278})}}>{/*should receive ID from props.movieID*/}
-    //     movie 2
-    //   </button>
-    //   <button onClick={()=>{setShow({show:true,movieID:573})}}>{/*should receive ID from props.movieID*/}
-    //     movie 3
-    //   </button>
-    //   <button onClick={()=>{setShow({show:true,movieID:3})}}>{/*should receive ID from props.movieID*/}
-    //     movie 4
-    //   </button>
-    //   <MovieModal showOption={showOption} setShow={setShow}/>
-    // </div>
-    <HomePage/>
+    <MovieList/>
   );
 }
 
