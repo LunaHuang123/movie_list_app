39c3bee106c190d2617c3527b0838918d240e114
Herman He <hjipe@DESKTOP-EU3DC6S.localdomain>
2022-09-28 17:08:19 -0400

    resolved conflicts

--- movie_list/src/components/sidebar/sidebar.jsx
+++ movie_list/src/components/sidebar/sidebar.jsx
@@ -6,10 +6,6 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import { LinkContainer } from 'react-router-bootstrap';
 import './sidebar.css';
 import { PAGE_ROUTE } from '../../constants/page_route';
-<<<<<<< HEAD
-=======
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import WhatshotOutlinedIcon from '@mui/icons-material/WhatshotOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import BlockOutlinedIcon from '@mui/icons-material/BlockOutlined';
 
@@ -30,20 +26,12 @@ function Logo() {
   );
 }
 
->>>>>>> 80d9ea37f5819e146c314e7d0baebfb3dd1fc48b
 
 export default function Sidebar() {
-  // Container fluid width:100% at all break point
   return (
-<<<<<<< HEAD
-    <Navbar bg='light' variant='light' expand={'lg'} className='mb-3' fixed='top' collapseOnSelect>
-      <Container fluid> 
-        <LinkContainer to={`/${PAGE_ROUTE.homePage}`}><Navbar.Brand><h1>Awesome List</h1></Navbar.Brand></LinkContainer>
-=======
     <Navbar bg='dark' variant='dark' expand={'lg'} className='mb-3' fixed='top' collapseOnSelect>
       <Container fluid> 
         <LinkContainer to={`/${PAGE_ROUTE.homePage}`}><Navbar.Brand><Logo /></Navbar.Brand></LinkContainer>
->>>>>>> 80d9ea37f5819e146c314e7d0baebfb3dd1fc48b
         <Navbar.Toggle aria-controls = 'offcanvasNavbar-expand' />
         <Navbar.Offcanvas
           id='offcanvasNavbar-expand'
@@ -52,30 +40,14 @@ export default function Sidebar() {
         >
           <Offcanvas.Header closeButton>
             <Offcanvas.Title id='offcanvasNavbarLabel-expand'>
-<<<<<<< HEAD
-              Awesome List
-=======
               <LinkContainer to='/homepage'>
                 <Navbar.Brand><Logo /></Navbar.Brand>
               </LinkContainer>
->>>>>>> 80d9ea37f5819e146c314e7d0baebfb3dd1fc48b
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className='justify-content-end flex-grow-1 pe-3'>
               <LinkContainer to={`/${PAGE_ROUTE.homePage}`}>
-<<<<<<< HEAD
-                <Nav.Link>Homepage</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to={`/${PAGE_ROUTE.topRatedPage}`}>
-                <Nav.Link>Top Rated Movies</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to={`/${PAGE_ROUTE.likedListPage}`}>
-                <Nav.Link>Liked Movies</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to={`/${PAGE_ROUTE.blockedListPage}`}>
-                <Nav.Link>Blocked Movies</Nav.Link>
-=======
                 <Nav.Link>
                   <HomeOutlinedIcon />
                   Home
@@ -98,7 +70,6 @@ export default function Sidebar() {
                   <BlockOutlinedIcon/>
                   Blocked Movies
                 </Nav.Link>
->>>>>>> 80d9ea37f5819e146c314e7d0baebfb3dd1fc48b
               </LinkContainer>
             </Nav>
           </Offcanvas.Body>
--- movie_list/src/reducers/root_reducer.js
+++ movie_list/src/reducers/root_reducer.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 80d9ea37f5819e146c314e7d0baebfb3dd1fc48b
+
 import { combineReducers } from "redux";
 import movieDetailsReducer from "./movieDetailsReducer";
 import topRatedPageReducer from "./topRatedPageReducer";
@@ -13,14 +10,5 @@ const rootReducer = combineReducers({
   popularPage: popularPageReducer,
   likeBlockLists: likeBlockReducer
 });
-
-
-<<<<<<< HEAD
 export default rootReducer;
-=======
 
-
->>>>>>> b6163e870a67a485689903a3b6f2b69f6a1cc2d2
-=======
-export default rootReducer;
->>>>>>> 80d9ea37f5819e146c314e7d0baebfb3dd1fc48b
