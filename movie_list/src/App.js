405b47f0059634ca47f66601905f85c150ef179b
Andonidas <yanyul4@uci.edu>
2022-09-27 17:15:41 -0700

    testing

--- movie_list/src/App.js
+++ movie_list/src/App.js
@@ -1,12 +1,31 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-
-import MovieList from './components/movieListComponent/layout' ;
+import TopRatedPage from './pages/top_rated_page';
+import MovieModal from './components/movie_modal';
+import Sidebar from './components/sidebar/sidebar';
+import LikedMoviesListPage from './pages/liked_movies_list_page';
+import BlockedMoviesListPage from './pages/blocked_movies_list_page';
+import HomePage from './components/homepage/homepage_container'
 
 function App() {
   const [showOption,setShow] = useState({show:false,movieID:null});
   return (
-    <MovieList/>
+    <div className="App">
+      <Router>
+        <Sidebar/>
+        <Routes>
+          <Route path='top_rated'>
+            <Route index element={<TopRatedPage />}/>
+            <Route path=':pageNumber' element={<TopRatedPage />}/>
+          </Route>
+          <Route path='liked_movies' element={<LikedMoviesListPage />}/>
+          <Route path='blocked_movies' element={<BlockedMoviesListPage />}/>
+        </Routes>
+      </Router>
+      <HomePage/>
+      <MovieModal showOption={showOption} setShow={setShow}/>
+    </div>
   );
 }
 
